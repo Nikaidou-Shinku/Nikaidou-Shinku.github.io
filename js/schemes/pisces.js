43cd55f5be66a0a535240eebfd28b697a7d0fb97
Nikaidou-Shinku <1106609918@qq.com>
2020-02-24 14:12:12 +0800

    Site updated: 2020-02-24 14:12:10

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Wed Feb 12 2020 22:05:35 GMT+0800 (GMT+08:00)
+// build time:Mon Feb 24 2020 14:11:32 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var i=CONFIG.sidebar.offset||12;function e(){return $(".header-inner").height()+i}function o(){var t=$("#footer");var i=$(".footer-inner");var e=t.outerHeight()-i.outerHeight();var o=t.outerHeight()+e;return o}function a(){var a=e();var f=o();t.affix({offset:{top:a-i,bottom:f}});t.affix("checkPosition");$("#sidebar").css({"margin-top":a,"margin-left":"auto"})}function f(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function r(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){f()}})}a();r()});
 //rebuild by neat 
